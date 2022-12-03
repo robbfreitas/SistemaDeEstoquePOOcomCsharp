d1d4f5ba87d0df893a96b6a4016fdb67af837986
robbfreitas <robertocompromissos@gmail.com>
2022-12-03 12:11:17 -0300

    Adicionado métodos de inserir e remover produtos.

--- Program.cs
+++ Program.cs
@@ -18,8 +18,24 @@ namespace SistemaDeEstoquePOOcomCsharp
             Console.WriteLine("Quantidade: ");
             p.Quantidade = int.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
 
+            Console.WriteLine();
             Console.WriteLine("Dados do produto: " + p);
 
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+
+            p.AdicionarProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+
+            p.RemoverProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
         }
     }
 }
