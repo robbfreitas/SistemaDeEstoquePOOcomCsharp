5302277f6c0018a821300f345d406439823c6a33
robbfreitas <robertocompromissos@gmail.com>
2022-12-03 11:33:14 -0300

    Add project files.

---
+++ Produto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeEstoquePOOcomCsharp
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", $ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $"
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace SistemaDeEstoquePOOcomCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre com os dados do produto:");
+
+            Console.WriteLine("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.WriteLine("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine("Quantidade: ");
+            p.Quantidade = int.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Dados do produto: " + p);
+
+        }
+    }
+}
